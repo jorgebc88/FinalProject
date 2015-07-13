c9a32d41bfedce7f38854194ccc42721d7c08a19
jorgebc88 <jorgebc88@gmail.com>
2015-07-12 23:05:14 -0300

    Remover librerias no usadas

--- src/main/java/com/finalproject/controller/UserController.java
+++ src/main/java/com/finalproject/controller/UserController.java
@@ -1,13 +1,10 @@
 package com.finalproject.controller;
 
-import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.map.JsonMappingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
